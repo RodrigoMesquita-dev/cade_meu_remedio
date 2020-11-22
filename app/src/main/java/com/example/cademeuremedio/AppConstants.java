684dd9cde61add464572526ad86dc2c9e2f65339
patricdayan <patricdayan@hotmail.com>
2020-11-22 01:37:27 -0400

    Primeiro commit

---
+++ app/src/main/java/com/example/cademeuremedio/AppConstants.java
@@ -0,0 +1,5 @@
+package com.example.cademeuremedio;
+
+public class AppConstants {
+    public static final int MAP_MAX_ZOOM = 1000;
+}
---
+++ app/src/main/java/com/example/cademeuremedio/LocationManager.java
@@ -0,0 +1,4 @@
+package com.example.cademeuremedio;
+
+public class LocationManager {
+}
